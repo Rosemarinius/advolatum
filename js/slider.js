bb7c07f9101594103c9b4ea197df1bb5a3681798
Rosemarinius <eddieshowyt2000@gmail.com>
2022-12-01 04:36:16 +0200

    #

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,31 @@
 new Swiper('.ts-slider', {
-    loop: true,
+  loop: true,
+  
+  pagination: {
+  el: '.ts-slider-pagination',
+  bulletClass: 'ts-slider-pag-bullet',
+  bulletActiveClass: 'ts-slider-pag-bullet--active',
+  renderBullet: function(index, className) {
+    return '<div class="' + className + '"></div>';
+  },
+  clickable: true,
+},
+});
 
-    pagination: {
-      el: '.ts-slider-pagination',
-      bulletClass: 'ts-slider-pag-bullet',
-      bulletActiveClass: 'ts-slider-pag-bullet--active',
-      renderBullet: function(index, className) {
-        return '<div class="' + className + '"></div>';
-      },
-      clickable: true,
+
+new Swiper('.actual-slider', {
+  breakpoints: {
+    0: {
+      slidesPerView: "auto",
     },
-  });
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 25,
+    },
+    1248: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+    
+  }
+})
