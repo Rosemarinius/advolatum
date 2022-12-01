c6f992221b7cd2ec56c52ded0811d5b90de823f9
Rosemarinius <eddieshowyt2000@gmail.com>
2022-12-02 01:50:17 +0200

    #

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+let itemInCart = 0;
+
+$(".header-burger").click(function() {
+    $(".burger-menu").slideDown("slow");
+    $("body").addClass("lock");
+})
+$(".burger-menu__close").click(function() {
+    $(".burger-menu").fadeOut("slow");
+    $("body").removeClass("lock");
+})
+
+
 $(".actual-card__photo").click(function() {
     $(this).closest(".actual-card-flip").css("transform", "rotateY(180deg)");
 })
