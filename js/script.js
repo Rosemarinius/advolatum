92e3e43a48ba5f2d78d9b67721ac7c7826400040
Rosemarinius <eddieshowyt2000@gmail.com>
2022-12-03 02:07:44 +0200

    #

--- js/script.js
+++ js/script.js
@@ -16,3 +16,58 @@ $(".actual-card__photo").click(function() {
 $(".actual-card__bg").click(function() {
     $(this).closest(".actual-card-flip").css("transform", "rotateY(0)");
 })
+
+
+
+$(".put-in-cart, .more").click(function() {
+    const card = this.closest(".product");
+
+    const productInfo = {
+        id: card.dataset.id,
+        img: card.querySelector(".product-image").getAttribute("src"),
+        productName: card.querySelector(".product-name").innerText,
+        price: card.querySelector(".product-price").innerText,
+    };
+
+    const productCardHTML = `<div class="product-card centered">
+<div class="product-card__image">
+    <img src="${productInfo.img}" alt="product-img">
+</div>
+<div class="product-card__info">
+    <i class="product-card__close icon-cross"></i>
+    <h3 class="product-card__price product-price">${productInfo.price}</h3>
+    <p class="product-card__description product-description">
+        Описание товара или состав
+        ткани, в общем любая информация
+        которая может потребоваться
+        покупателю</p>
+    <ul class="product-card__sizes">
+        <li class="product-card__size hover-yellow">XS</li>
+        <li class="product-card__size hover-yellow">S</li>
+        <li class="product-card__size hover-yellow">M</li>
+        <li class="product-card__size hover-yellow">L</li>
+        <li class="product-card__size hover-yellow">XL</li>
+    </ul>
+    <ul class="product-card__colors">
+        <li class="product-card__color"><img src="img/svg/color-white.svg" alt="purple"></li>
+        <li class="product-card__color"><img src="img/svg/color-green.svg" alt="purple"></li>
+        <li class="product-card__color"><img src="img/svg/color-blue.svg" alt="purple"></li>
+        <li class="product-card__color"><img src="img/svg/color-purple.svg" alt="purple"></li>
+        <li class="product-card__color"><img src="img/svg/color-yellow.svg" alt="purple"></li>
+    </ul>
+    <div class="product-cart-button-wrapper">
+        <button class="product-card__button product-card-button button">
+            <img src="img/svg/wing.svg" alt="wing">
+            <a class="product-card-button__text link-reset">В корзину</a>
+        </button>
+    </div>
+</div>
+</div>`
+
+    $(".dark-bg-product-card").css("display", "flex").fadeIn("slow").append(productCardHTML);
+    
+    $(".product-card__close").click(function() {
+        $(".dark-bg-product-card").fadeOut("slow");
+        $(".product-card").remove();
+    })
+});
